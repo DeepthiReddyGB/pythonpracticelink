4a60afaf4569b01f0f787bc59ad784c146eec9de
DeepthiReddyGB <deepthireddy0711@gmail.com>
2021-04-23 19:19:32 +0530

    COMMIT

---
+++ ifelse/abbrivation.py
@@ -0,0 +1,10 @@
+t = input('Enter abbreviation from tweet:\n')
+
+if t == 'LOL':
+    print('LOL = laughing out loud')
+elif t == 'rofl':
+    print('rofl=rolling on the floor laughing')
+elif t == 'lmk':
+    print('lmk=let me know')
+elif t == 'smh':
+    print('smh=shaking my head')
